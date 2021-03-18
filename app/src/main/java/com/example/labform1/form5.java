59a0810da0a3ff5a7cb9459a627a1537f6b51a75
Your Name <thanhhong593@gmail.com>
2021-03-18 23:20:35 +0700

    first commit

---
+++ app/src/main/java/com/example/labform1/form5.java
@@ -0,0 +1,14 @@
+package com.example.labform1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class form5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form5);
+    }
+}
